f3dccb784d80e6a2b10e10e2a076383fdc9d3217
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-06-17 13:07:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+names=["kim",'moss','jane', 'mbatia']
+//events=[]
+
+function writeCards(names){
+    let events=[]
+    for(let i=0;i<names.length;i++){
+        events.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+        //console.log(events)
+    }
+    return events
+}
+
+
+function countDown(int){
+    let i=0;
+    while( i<=int){
+        
+        console.log(int)
+        int--
+    }
+}
+
+writeCards(names)
+console.log(events)
+
+//countDown(4);
